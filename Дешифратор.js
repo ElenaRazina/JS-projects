3d57a55fe96fe40e56c5d52a13c48aec96703eec
Elena Razina <114656349+ElenaRazina@users.noreply.github.com>
2022-10-25 12:40:19 +0300

    Add files via upload

---
+++ Дешифратор.js
@@ -0,0 +1,7 @@
+let encryption= "Ybrobrubr brabrrbrebr brtbrhbrebr brbbrebrsbrtbr brQbrAbr brebrvbrebrrbr";
+let transcrip = "";
+for (let i=0; i<encryption.length;i+=3)
+{
+    transcrip +=encryption[i];
+}
+console.log(transcrip);
